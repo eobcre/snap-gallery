53d3b4ed0b10e9402f7b7879bb406ae49c3d2546
eobcre <eobcre@gmail.com>
2023-04-20 23:20:12 -0400

    img data

---
+++ src/imgData.jsx
@@ -0,0 +1,32 @@
+export const imgData = [
+  {
+    src: '/assets/hawaii_001.jpg',
+  },
+  {
+    src: '/assets/la_001.jpg',
+  },
+  {
+    src: '/assets/hawaii_002.jpg',
+  },
+  {
+    src: '/assets/la_002.jpg',
+  },
+  {
+    src: '/assets/hawaii_003.jpg',
+  },
+  {
+    src: '/assets/la_003.jpg',
+  },
+  {
+    src: '/assets/hawaii_004.jpg',
+  },
+  {
+    src: '/assets/la_004.jpg',
+  },
+  {
+    src: '/assets/hawaii_005.jpg',
+  },
+  {
+    src: '/assets/la_005.jpg',
+  },
+];
