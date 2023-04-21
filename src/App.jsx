76f15fe129ffaec185d63b41dc49f82d0feb2d4c
eobcre <eobcre@gmail.com>
2023-04-20 23:21:25 -0400

    img loop logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { imgData } from './imgData';
+
 const App = () => {
   return (
     <>
       <h1># Snap Gallery</h1>
+      <div className='img-container'>
+        {imgData.map(({ src }, index) => (
+          <img
+            key={src}
+            src={src}
+            alt={`Image ${index}`}
+            width='320'
+            height='240'
+          />
+        ))}
+      </div>
     </>
   );
 };
