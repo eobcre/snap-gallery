2f67aa2752a6054d9c83feab3174fe8d4c0e903c
eobcre <eobcre@gmail.com>
2023-04-21 23:35:55 -0400

    created drop down

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,7 +1,14 @@
-import { imgData } from '../imgData';
-
 const Images = () => {
-  return <div></div>;
+  return (
+    <div>
+      <label htmlFor='category'>Select the location</label>
+      <select>
+        <option value='all'>Show all</option>
+        <option value='category1'>Hawaii</option>
+        <option value='category2'>Los Angeles</option>
+      </select>
+    </div>
+  );
 };
 
 export default Images;
