99c6da5d123898512a47b21be7e22444b359c2b0
eobcre <eobcre@gmail.com>
2023-04-21 23:48:12 -0400

    className dropdown, dropdown-title created

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,12 +1,16 @@
 const Images = () => {
   return (
     <div>
-      <label htmlFor='category'>Select the location</label>
-      <select>
-        <option value='all'>Show all</option>
-        <option value='category1'>Hawaii</option>
-        <option value='category2'>Los Angeles</option>
-      </select>
+      <div className='dropdown'>
+        <label htmlFor='category' className='dropdown-title'>
+          Select the location
+        </label>
+        <select>
+          <option value='all'>Show all</option>
+          <option value='category1'>Hawaii</option>
+          <option value='category2'>Los Angeles</option>
+        </select>
+      </div>
     </div>
   );
 };
