fd80a55ec7bfb83464919eb1ee6010be57ba67d1
eobcre <eobcre@gmail.com>
2023-04-21 22:59:40 -0400

    new property added

--- src/imgData.jsx
+++ src/imgData.jsx
@@ -1,32 +1,42 @@
 export const imgData = [
   {
     src: '/assets/hawaii_001.jpg',
+    category: 'category1',
   },
   {
     src: '/assets/la_001.jpg',
+    category: 'category2',
   },
   {
     src: '/assets/hawaii_002.jpg',
+    category: 'category1',
   },
   {
     src: '/assets/la_002.jpg',
+    category: 'category2',
   },
   {
     src: '/assets/hawaii_003.jpg',
+    category: 'category1',
   },
   {
     src: '/assets/la_003.jpg',
+    category: 'category2',
   },
   {
     src: '/assets/hawaii_004.jpg',
+    category: 'category1',
   },
   {
     src: '/assets/la_004.jpg',
+    category: 'category2',
   },
   {
     src: '/assets/hawaii_005.jpg',
+    category: 'category1',
   },
   {
     src: '/assets/la_005.jpg',
+    category: 'category2',
   },
 ];
