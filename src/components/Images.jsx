22941257a1a5ab6eb4d863dcd4c35772d3ed8daa
eobcre <eobcre@gmail.com>
2023-04-23 22:14:25 -0400

    label removed

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -19,9 +19,6 @@ const Images = () => {
   return (
     <>
       <div className='dropdown'>
-        <label htmlFor='category' className='dropdown-title'>
-          Select the location
-        </label>
         <select value={category} onChange={handleChange}>
           <option value='all'>Show all</option>
           <option value='category1'>Hawaii</option>
